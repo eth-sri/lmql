a06348ae636dc84dc0e3aad902d808afd209989d
Markus <44006014+minosvasilias@users.noreply.github.com>
2023-06-16 20:49:59 +0200

    Add 4bit support

--- src/lmql/models/lmtp/utils.py
+++ src/lmql/models/lmtp/utils.py
@@ -5,7 +5,9 @@ def rename_model_args(model_args):
     cuda = model_args.pop("cuda", False)
     dtype = model_args.pop("dtype", None)
 
-    if dtype == "8bit":
+    if dtype == "4bit":
+        model_args["load_in_4bit"] = True
+    elif dtype == "8bit":
         model_args["load_in_8bit"] = True
     elif dtype is not None:
         model_args["torch_dtype"] = getattr(torch, dtype)
