3d8776042ce4ee130affb13b8576bee702eea097
Luca Beurer-Kellner <lucabeurerkellner@gmail.com>
2023-07-14 12:24:36 +0200

    auto-publishing CI workflow (#112)

--- scripts/pypi-release.sh
+++ scripts/pypi-release.sh
@@ -1,13 +1,13 @@
 if [ "$2" == "--production" ]; then
     echo "[WARNING] Uploading to production PyPI"
     echo "Source Distribution"
-    python -m twine upload dist/$1.tar.gz
+    python -m twine upload dist/$1.tar.gz -u $TWINE_USERNAME -p $TWINE_PASSWORD
     echo "Wheel Distribution"
-    python -m twine upload dist/$1-py3-none-any.whl
+    python -m twine upload dist/$1-py3-none-any.whl -u $TWINE_USERNAME -p $TWINE_PASSWORD
 else
     echo "Uploading to test.pypi.org"
     echo "Source Distribution"
-    python -m twine upload --repository testpypi dist/$1.tar.gz
+    python -m twine upload --repository testpypi dist/$1.tar.gz -u $TWINE_USERNAME -p $TWINE_PASSWORD
     echo "Wheel Distribution"
-    python -m twine upload --repository testpypi dist/$1-py3-none-any.whl
+    python -m twine upload --repository testpypi dist/$1-py3-none-any.whl -u $TWINE_USERNAME -p $TWINE_PASSWORD
 fi
