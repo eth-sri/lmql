ee20fd069d9e0c3d5ba0759d199dcea4e6d00d7d
Carlo Nucera <carlo.nucera@protonmail.com>
2023-07-19 12:31:54 +0000

    Add support for GPTQ quantization
    This PR addresses the need to use GPTQ-quantized models, which can't currently
    be loaded with the transformers library.
    
    It introduces an optional `--loader` flag with support for the `auto-gptq`
    option. When this option is specified, the (quantized) model will be loaded
    using the `auto_gptq` library.
    
    To serve the model, run the following command:
    ```
    lmql serve-model TheBloke/WizardCoder-15B-1.0-GPTQ --cuda --loader auto-gptq --use_safetensors true
    ```
    
    Or use the in-process invocation with:
    ```
    from lmql.model("local:TheBloke/WizardCoder-15B-1.0-GPTQ", cuda=True, loader="auto-gptq", use_safetensors=True)
    ```

--- src/lmql/models/lmtp/backends/transformers_model.py
+++ src/lmql/models/lmtp/backends/transformers_model.py
@@ -1,4 +1,3 @@
-from transformers import AutoModelForCausalLM
 from typing import Tuple
 import torch
 from lmql.models.lmtp.backends.lmtp_model import LMTPModel, LMTPModelResult, TokenStreamer
@@ -9,9 +8,14 @@ class TransformersLLM(LMTPModel):
         self.model_identifier = model_identifier
         self.model_args = kwargs
 
-        print("[Loading", self.model_identifier, "with", f"AutoModelForCausalLM.from_pretrained({self.model_identifier}, {str(self.model_args)[1:-1]})]", flush=True)
-        
-        self.model = AutoModelForCausalLM.from_pretrained(self.model_identifier, **kwargs)
+        if self.model_args.pop("loader", None) == "auto-gptq":
+            from auto_gptq import AutoGPTQForCausalLM
+            print("[Loading", self.model_identifier, "with", f"AutoGPTQForCausalLM.from_quantized({self.model_identifier}, {str(self.model_args)[1:-1]})]", flush=True)
+            self.model = AutoGPTQForCausalLM.from_quantized(self.model_identifier, **self.model_args)
+        else:
+            from transformers import AutoModelForCausalLM
+            print("[Loading", self.model_identifier, "with", f"AutoModelForCausalLM.from_pretrained({self.model_identifier}, {str(self.model_args)[1:-1]})]", flush=True)
+            self.model = AutoModelForCausalLM.from_pretrained(self.model_identifier, **self.model_args)
         
         print("[", self.model_identifier, " ready on device ", self.model.device, 
         flush=True, sep="", end="]\n")
