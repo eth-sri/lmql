b739bc61deeb54c0f0f7e491ec6741abb2430ce9
Leon Knauer <git@leonknauer.com>
2023-07-24 21:18:25 +0200

    Fixed broken lmql command when using zsh-autoswitch-virtualenv plugin.

--- scripts/activate-dev.sh
+++ scripts/activate-dev.sh
@@ -1,5 +1,7 @@
 # use this script to run 'lmql' commands in the local development copy of LMQL
-ABSOLUTE_LMQL_PATH=$(cd $(dirname $0)/.. && pwd)
+cd $(dirname $0)/.. > /dev/null
+ABSOLUTE_LMQL_PATH=$(pwd)
+cd - > /dev/null
 echo "Using LMQL distribution in $ABSOLUTE_LMQL_PATH"
 export PYTHONPATH=$ABSOLUTE_LMQL_PATH/src
 alias lmql="PYTHONPATH=$ABSOLUTE_LMQL_PATH/src python -m lmql.cli \$*"
