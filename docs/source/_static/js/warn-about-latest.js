bdc277bc875bfe5adaf0be514e745e9b59312a2c
Luca Beurer-Kellner <lucabeurerkellner@gmail.com>
2023-09-08 19:32:16 +0200

    docs: warn about latest message

--- docs/source/_static/js/warn-about-latest.js
+++ docs/source/_static/js/warn-about-latest.js
@@ -3,7 +3,7 @@
     if (!window.location.pathname.includes('/stable/')) {
         var warning = document.createElement('DIV');
         warning.className = 'note latest';
-        warning.innerHTML = '<b>Note</b> You are viewing the <i>latest version</i> of the docs. This means some documented features may not be available in the version you are using. You can view the <a href="/en/stable/">stable version instead </a> or make sure you are running the <a href="https://github.com/eth-sri/lmql#installing-the-latest-development-version">latest LMQL version.</a>';
+        warning.innerHTML = '<b>Note:</b> This is the <i>latest version</i> of the documentation (preview releases or <code>main</code> branch). You can view the <a href="/en/stable/">stable version of the documentation instead </a> or make sure you are running the <a href="https://github.com/eth-sri/lmql#installing-the-latest-development-version">latest LMQL version.</a>';
         warning.classList.add('sy-alert', 'sy-alert--warning');
         document.querySelector('article[role=main]').prepend(warning);
     }
