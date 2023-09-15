80f0f32ee5dc3e4550ced8657ddd250c0d5f8fd9
Khush Chopra <khush@Khushs-MacBook-Pro.local>
2023-09-16 00:31:14 +0530

    Fix sentencepiece misbehavior of starting space

--- src/lmql/runtime/tokenizers/llama_cpp_tokenizer.py
+++ src/lmql/runtime/tokenizers/llama_cpp_tokenizer.py
@@ -1,73 +1,11 @@
-
-"""
-
-
-
-class LlamaCPPTokenizer:
-    def __init__(self, model_identifier):
-        from transformers import LlamaTokenizer
-        self.model_identifier = model_identifier
-        self.tokenizer = LlamaTokenizer(model_identifier)
-
-    @staticmethod
-    def is_available(model_identifier):
-        # Returns True if this tokenizer implementation is available for the given model identifier.
-        try:
-            from transformers import LlamaTokenizer
-            return True
-        except ImportError:
-            return False
-    
-    def __getstate__(self):
-        return {
-            "model_identifier": self.model_identifier,
-            "tokenizer": self.tokenizer
-        }
-    
-    def __setstate__(self, state):
-        self.model_identifier = state["model_identifier"]
-        self.tokenizer = state["tokenizer"]
-    
-    def tokenize(self, s, asbytes=False):
-        if s == " " or s == "\n":
-            return "_" if s==" " else "<0x0A>"
-        print("SSSss ", s)
-        print(asbytes)
-        if asbytes:
-            ids = self(s)["input_ids"]
-            return self.decode_tokens_bytes(ids)
-        else:
-            r = self.tokenizer.tokenize(s)
-            print("ASDA", r)
-            return r
-
-    @property
-    def vocab(self):
-        return self.tokenizer.vocab
-
-    @property
-    def vocab_size(self):
-        return self.tokenizer.vocab_size
-
-    @property
-    def eos_token_id(self):
-        return self.tokenizer.eos_token_id()
-
-    @property
-    def bos_token_id(self):
-        return self.tokenizer.bos_token_id()
-    
-    @property
-    def name(self):
-        return "llama.cpp-" + self.model_identifier
-    
-
-"""
 class LlamaCPPTokenizer:
     def __init__(self, model_identifier):
         from sentencepiece import SentencePieceProcessor
         self.model_identifier = model_identifier
         self.tokenizer = SentencePieceProcessor(model_identifier)
+        
+    def get_token_by_ids(self, ids):
+        return self.tokenizer.id_to_piece(ids)
 
     @staticmethod
     def is_available(model_identifier):
@@ -86,22 +24,17 @@ class LlamaCPPTokenizer:
     
     def __setstate__(self, state):
         from sentencepiece import SentencePieceProcessor
-        
         self.model_identifier = state["model_identifier"]
         self.tokenizer = state["tokenizer"]
     
-    def tokenize(self, s, asbytes=False):
-        print("SSSss ", s)
-        print(asbytes)
-
-        if s == " " or s == "\n":
-            return ["_"] if s==" " else ["<0x0A>"]
+    def tokenize(self, text, asbytes=False, add_special_tokens=False):
+        if text == " " or text == "\n":
+            return ["▁"] if text==" " else ["<0x0A>"]
         if asbytes:
-            ids = self(s)["input_ids"]
+            ids = self(text)["input_ids"]
             return self.decode_tokens_bytes(ids)
-        else:
-            return self.tokenizer.tokenize(s)
-        
+        return self.tokenizer.tokenize(text)
+    
     def decode_tokens_bytes(self, ids):
         """
         Converts a list of token ids into a list of token bytes.
@@ -110,7 +43,6 @@ class LlamaCPPTokenizer:
         return [f"{i}".encode("utf-8") for i in ids]
 
     def __call__(self, text, add_special_tokens=False):
-        # text_to_tokenize = "<s>" + text_to_tokenize
         result =  self.tokenizer.encode(text)
         return {
             "input_ids": result
@@ -122,17 +54,22 @@ class LlamaCPPTokenizer:
 
         It must hold that self.convert_bytes_to_string(self.decode_tokens_bytes(ids)) == self.decode(ids).
         """
+        if len(token_bytes)==1:
+            tokens = self.get_token_by_ids([int(token.decode("utf-8")) for token in token_bytes])
+            if tokens[0].startswith("▁"):
+                ids = self.convert_token_bytes_to_ids(token_bytes)
+                res = self.tokenizer.decode(ids)
+                return f" {res}"
         ids = self.convert_token_bytes_to_ids(token_bytes)
         res = self.tokenizer.decode(ids)
-        print("Input tokens", token_bytes)
-        print("Output res", res)
         return res
     
     def decode(self, ids, clean_up_tokenization_spaces=True):
         """
         Converts a list of token ids into a string.
         """
-        return self.tokenizer.decode(ids)
+        res = self.tokenizer.decode(ids)
+        return res
     
     def convert_token_bytes_to_ids(self, tokens):
         """
@@ -141,7 +78,7 @@ class LlamaCPPTokenizer:
         Inverse of self.decode_tokens_bytes.
         """
         return [int(t.decode("utf-8")) for t in tokens]
-
+    
     @property
     def vocab(self):
         return { self.tokenizer.id_to_piece(id): id for id in range(self.tokenizer.get_piece_size()) }
@@ -153,7 +90,7 @@ class LlamaCPPTokenizer:
     @property
     def eos_token_id(self):
         return self.tokenizer.eos_id()
-
+    
     @property
     def bos_token_id(self):
         return self.tokenizer.bos_id()
