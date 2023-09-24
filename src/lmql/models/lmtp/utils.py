6afb55a216b451f5374b118cea8026124b622ef6
ambrose <ambroser53@gmail.com>
2023-09-24 16:07:00 +0100

    adjustments to model naming arguments for adding quantization configuration options, specifying datatype of the model as separate to the selection of Nbit quantisation.
    Current method is rather hacky with the passing of quantization parameters as a frozenset. Alternative approaches for the same outcome are welcome.

--- src/lmql/models/lmtp/utils.py
+++ src/lmql/models/lmtp/utils.py
@@ -1,21 +1,33 @@
+from transformers import BitsAndBytesConfig
 import warnings
+
 
 def rename_model_args(model_args):
     cuda = model_args.pop("cuda", False)
+    bits = model_args.pop("bits", 16)
     dtype = model_args.pop("dtype", None)
+    q_config = model_args.pop("quantization_config", None)
 
-    if dtype == "4bit":
+    if bits == 4:
         model_args["load_in_4bit"] = True
-    elif dtype == "8bit":
+    elif bits == 8:
         model_args["load_in_8bit"] = True
-    elif dtype is not None:
+
+    if dtype is not None:
         import torch
         model_args["torch_dtype"] = getattr(torch, dtype)
+
+    if type(q_config) is set:
+        q_config = {k: v for k, v in q_config.pop()}
+        model_args["quantization_config"] = BitsAndBytesConfig(**q_config)
+    elif q_config is not None:
+        warnings.warn(
+            "quantization_config is not a frozenset, so it will not be used. Use frozenset(Create BitsAndBytesConfig.__dict__.items()) instead.")
 
     # parse cuda
     if cuda:
         if "device_map" in model_args:
-            warnings.warn("Warning: device_map is set, but cuda is True. Ignoring 'cuda' which would set device_map to 'auto'.")
+            print("Warning: device_map is set, but cuda is True. Ignoring 'cuda' which would set device_map to 'auto'.")
         else:
             model_args["device_map"] = "auto"
 
