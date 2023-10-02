db30d0d5bce872c230ba60b67e8574790ab198ea
Luca Beurer-Kellner <luca.beurer-kellner@inf.ethz.ch>
2023-10-02 18:47:19 +0200

    openai: avoid cache misses

--- src/lmql/runtime/openai_integration.py
+++ src/lmql/runtime/openai_integration.py
@@ -426,6 +426,10 @@ class DclibOpenAiModel(DcModel):
         return await asyncio.gather(*[get_buffer(i, s) for i, s in enumerate(seqs)])
 
     async def expand_and_cache(self, s: DecoderSequence, completion_result: CompletionResult, sampling_mode, logprobs=1):
+        # wait at least for the first completion so 
+        # the cache is guaranteed to be ahead
+        res = await completion_result.buffer.get(0)
+
         async def token_stream():
             nonlocal sampling_mode, s, completion_result
             response_buffer = completion_result.buffer
