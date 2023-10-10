373bfb36f2fbb53e81d97e2be56ec363ece54858
lbeurerkellner <lucabeurerkellner@gmail.com>
2023-10-10 15:20:10 +0000

    Deploying to web from @ eth-sri/lmql@9d78fbcdc48bf4776c684546eb803a4ceeba69c5 🚀

--- assets/features_examples_2-constraining.md.6f6c70eb.lean.js
+++
@@ -1,9 +0,0 @@
-import{_ as n,o as e,c as t,Q as o,k as s,a}from"./chunks/framework.c2adf1ba.js";const j=JSON.parse('{"title":"⛓️ Constrained LLMs","description":"","frontmatter":{"title":"⛓️ Constrained LLMs"},"headers":[],"relativePath":"features/examples/2-constraining.md","filePath":"features/examples/2-constraining.md"}'),p={name:"features/examples/2-constraining.md"},l=o("",4),r=s("div",{class:"language-promptdown vp-adaptive-theme"},[s("button",{title:"Copy Code",class:"copy"}),s("span",{class:"lang"},"promptdown"),s("pre",{"pd-text":`Tell me a joke:
-
-Q: [JOKE| What did the fish say when it hit the wall?]
-A: [PUNCHLINE| Dam!]
-`,animate:"true",__animate:"true","animate-speed":"50",class:"promptdown promptdown-compiled",style:{opacity:"1"}},[s("p",{"pd-shadow-id":"2000",text:"T","pd-insertion-point":"true"},[a(`Tell me a joke:
-
-Q: `),s("span",{"pd-shadow-id":"2002","pd-instant":"false",text:"",class:"promptdown-var color-lightorange"},[s("span",{"pd-shadow-id":"2003",text:"J",class:"promptdown-var-name"},"JOKE"),a(" What did the fish say when it hit the wall?")]),a(`
-A: `),s("span",{"pd-shadow-id":"2008","pd-instant":"false",text:"",class:"promptdown-var color-orange"},[s("span",{"pd-shadow-id":"2009",text:"P",class:"promptdown-var-name"},"PUNCHLINE"),a(" Dam")]),a(`
-`)])])],-1),i=[l,r];function c(d,h,m,_,u,g){return e(),t("div",null,i)}const f=n(p,[["render",c]]);export{j as __pageData,f as default};
