7342eca9f9e8b1f27df0bfe50632eccdd7deb0c3
lbeurerkellner <lucabeurerkellner@gmail.com>
2023-10-14 16:39:04 +0000

    Deploying to web from @ eth-sri/lmql@9a59739d980a88e64687689b7cecee6cbaf79b78 🚀

---
+++ assets/features_examples_3-multi-part.md.24944e25.lean.js
@@ -0,0 +1,15 @@
+import{_ as t,o as e,c as n,Q as o,k as s,a}from"./chunks/framework.4636910e.js";const A=JSON.parse('{"title":"🧠 Multi-Part Prompts","description":"","frontmatter":{"title":"🧠 Multi-Part Prompts"},"headers":[],"relativePath":"features/examples/3-multi-part.md","filePath":"features/examples/3-multi-part.md"}'),p={name:"features/examples/3-multi-part.md"},r=o("",4),l=s("div",{class:"language-promptdown vp-adaptive-theme"},[s("button",{title:"Copy Code",class:"copy"}),s("span",{class:"lang"},"promptdown"),s("pre",{"pd-text":`Q: It was Sept. 1st, 2021 a week ago. What is the date 10 days ago in MM/DD/YYYY?
+Answer Choices: (A) 08/29/2021 (B) 08/28/2021 (C) 08/29/1925 (D) 08/30/2021 (E) 05/25/2021 (F) 09/19/2021
+
+A: Let's think step by step.
+[REASONING(color='red')| Sept. 1st, 2021 was a week ago, so 10 days ago would be 8 days before that, which is August 23rd, 2021, so the answer is (A) 08/29/2021.]
+
+Therefore, the answer is [ANSWER| A]
+`,animate:"true",__animate:"true","animate-speed":"50",class:"promptdown promptdown-compiled",style:{opacity:"1"}},[s("p",{"pd-shadow-id":"2000",text:"Q","pd-insertion-point":"true"},[a(`Q: It was Sept. 1st, 2021 a week ago. What is the date 10 days ago in MM/DD/YYYY?
+Answer Choices: (A) 08/29/2021 (B) 08/28/2021 (C) 08/29/1925 (D) 08/30/2021 (E) 05/25/2021 (F) 09/19/2021
+
+A: Let's think step by step.
+`),s("span",{"pd-shadow-id":"2002","pd-instant":"false",text:"",class:"promptdown-var color-lightorange"},[s("span",{"pd-shadow-id":"2003",text:"R",class:"promptdown-var-name"},"REASONING"),a(" Sept. 1st, 2021 was a week ago, so 10 days ago would be 8 days before that, which is August 23rd, 2021, so the answer is (A) 08/29/2021.")]),a(`
+
+Therefore, the answer is `),s("span",{"pd-shadow-id":"2008","pd-instant":"false",text:"",class:"promptdown-var color-ochre"},[s("span",{"pd-shadow-id":"2009",text:"A",class:"promptdown-var-name"},"ANSWER"),a(" A")]),a(`
+`)])])],-1),i=[r,l];function c(d,h,u,m,w,g){return e(),n("div",null,i)}const q=t(p,[["render",c]]);export{A as __pageData,q as default};
