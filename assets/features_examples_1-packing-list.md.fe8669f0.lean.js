6cfdfbe163cc710cfc6c39c92bbbb86f588c0449
lbeurerkellner <lucabeurerkellner@gmail.com>
2024-02-14 21:54:29 +0000

    Deploying to web from @ eth-sri/lmql@a9bd19130477d5498663441ee500b0aafefeff07 🚀

---
+++ assets/features_examples_1-packing-list.md.fe8669f0.lean.js
@@ -0,0 +1,13 @@
+import{_ as n,o as t,c as e,Q as p,k as s,a}from"./chunks/framework.980cae92.js";const T=JSON.parse('{"title":"🌴 Packing List","description":"","frontmatter":{"title":"🌴 Packing List"},"headers":[],"relativePath":"features/examples/1-packing-list.md","filePath":"features/examples/1-packing-list.md"}'),o={name:"features/examples/1-packing-list.md"},l=p("",4),r=s("div",{class:"language-promptdown vp-adaptive-theme"},[s("button",{title:"Copy Code",class:"copy"}),s("span",{class:"lang"},"promptdown"),s("pre",{"pd-text":`My packing list for the trip:
+
+- [THING| Volleyball]
+- [THING| Bathing Suit]
+- [THING| Sunscreen]
+- [THING| Volleyball]
+`,animate:"true",__animate:"true","animate-speed":"50",class:"promptdown promptdown-compiled",style:{opacity:"1"}},[s("p",{"pd-shadow-id":"1958",text:"M","pd-insertion-point":"true"},[a(`My packing list for the trip:
+
+- `),s("span",{"pd-shadow-id":"1960","pd-instant":"false",text:"",class:"promptdown-var color-lightorange"},[s("span",{"pd-shadow-id":"1961",text:"T",class:"promptdown-var-name"},"THING"),a(" Volleyball")]),a(`
+- `),s("span",{"pd-shadow-id":"1966","pd-instant":"false",text:"",class:"promptdown-var color-lightorange"},[s("span",{"pd-shadow-id":"1967",text:"T",class:"promptdown-var-name"},"THING"),a(" Bathing Suit")]),a(`
+- `),s("span",{"pd-shadow-id":"1972","pd-instant":"false",text:"",class:"promptdown-var color-lightorange"},[s("span",{"pd-shadow-id":"1973",text:"T",class:"promptdown-var-name"},"THING"),a(" Sunscreen")]),a(`
+- `),s("span",{"pd-shadow-id":"1978","pd-instant":"false",text:"",class:"promptdown-var color-lightorange"},[s("span",{"pd-shadow-id":"1979",text:"T",class:"promptdown-var-name"},"THING"),a(" Volleyball")]),a(`
+`)])])],-1),i=[l,r];function c(d,m,h,u,g,_){return t(),e("div",null,i)}const f=n(o,[["render",c]]);export{T as __pageData,f as default};
